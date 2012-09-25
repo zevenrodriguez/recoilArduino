d88bc9d55fa5791d5bff4c09e4d21729412ce8a9
Zevensuy Rodriguez <zevenwolf@gmail.com>
2012-09-25 10:45:16 -0400

    servo

--- recoil_1/recoil_1.ino
+++ recoil_1/recoil_1.ino
@@ -1,4 +1,8 @@
+#include <Servo.h>
 
+Servo redServo;
+int redPos = 0;
+boolean redServoReady = false;
 
 
 long redSavedTime = 0;
@@ -19,14 +23,19 @@ long whiteTotalTime = 0;
 int randomMax = 5;
 int randomMin = 10;
 
+boolean redLight = true;
+
 void setup(){
 
   randomSeed(analogRead(0));
-
+  redServo.attach(2);
+  /*
   redTotalTime = random(randomMax,randomMin) * 1000;
-  blueTotalTime = random(randomMax,randomMin) * 1000; 
-  greenTotalTime = random(randomMax,randomMin) * 1000;
-  whiteTotalTime = random(randomMax,randomMin) * 1000;
+   blueTotalTime = random(randomMax,randomMin) * 1000; 
+   greenTotalTime = random(randomMax,randomMin) * 1000;
+   whiteTotalTime = random(randomMax,randomMin) * 1000;
+   */
+  redTotalTime = 10000;
 
   Serial.begin(9600);
 
@@ -65,91 +74,123 @@ void establishContact() {
 
 void launch(){
   //Red Section 
- if(Serial.read() == 5){ 
-  int redPassedTime = millis() - redSavedTime;
 
-  if (redPassedTime > redTotalTime) {
+  if(Serial.read() == 5){ 
 
-    //  Serial.println("red");
-    //Start servo
-    
-    //is servo ready
-    Serial.write(1);
+    redLight = true;
 
-
-    redSavedTime = millis();
 
   }
- }
+  if(redLight == true ){
+    int redPassedTime = millis() - redSavedTime;
 
-/*
+    if (redPassedTime > redTotalTime) {
+
+      //redServoReady = redServoMove();
+      redServoMove();
+      if (redServoReady == true){
+        for(int i = 0; i<10; i++){
+          Serial.write(1);
+        }
+      }
+      redSavedTime = millis();
+      redLight = false;
+
+    }
+
+  }
+  /*
   //Blue Section  
-  int bluePassedTime = millis() - blueSavedTime;
-
-  if (bluePassedTime > blueTotalTime) {
-
-    //Serial.println("blue");
-    Serial.write(2);
-
-    blueSavedTime = millis();
-
-
-  }
-  else{
-
-    Serial.write(10); 
-
-  }
-
-  //Green Section  
-  int greenPassedTime = millis() - greenSavedTime;
-
-  if (greenPassedTime > greenTotalTime) {
-
-    // Serial.println("green");
-    Serial.write(3);
-
-    greenSavedTime = millis();
-
-  }else{
+   int bluePassedTime = millis() - blueSavedTime;
+   
+   if (bluePassedTime > blueTotalTime) {
+   
+   //Serial.println("blue");
+   Serial.write(2);
+   
+   blueSavedTime = millis();
+   
+   
+   }
+   else{
    
    Serial.write(10); 
-    
-  }
-
-  //White Section  
-  int whitePassedTime = millis() - whiteSavedTime;
-
-  if (whitePassedTime > whiteTotalTime) {
-
-    //Serial.println("white");
-    Serial.write(4);
-
-    whiteSavedTime = millis();
-
-
-
-  }else{
+   
+   }
+   
+   //Green Section  
+   int greenPassedTime = millis() - greenSavedTime;
+   
+   if (greenPassedTime > greenTotalTime) {
+   
+   // Serial.println("green");
+   Serial.write(3);
+   
+   greenSavedTime = millis();
+   
+   }else{
    
    Serial.write(10); 
-    
-  }
-*/
+   
+   }
+   
+   //White Section  
+   int whitePassedTime = millis() - whiteSavedTime;
+   
+   if (whitePassedTime > whiteTotalTime) {
+   
+   //Serial.println("white");
+   Serial.write(4);
+   
+   whiteSavedTime = millis();
+   
+   
+   
+   }else{
+   
+   Serial.write(10); 
+   
+   }
+   */
 
 }
 
 
 
 
-void servoMove(int servo){
-  
-  //which servo do I move
-  
-  //move servo
-  
- //run timer when done set to true
+boolean redServoMove(){
+
+  redServo.write(0);
+  delay(500);
+  for(redPos = 0; redPos < 45; redPos += 1)  // goes from 0 degrees to 180 degrees 
+  {  
+    redServo.write(redPos);
+    delay(10);
+  }
+
+  for(redPos = 45; redPos>=1; redPos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    redServo.write(redPos);              // tell servo to go to redPosition in variable 'redPos' 
+    delay(15);                       // waits 15ms for the servo to reach the redPosition 
+  }
+
+  for(redPos = 0; redPos < 90; redPos += 1)  // goes from 0 degrees to 180 degrees 
+  {  
+    redServo.write(redPos);
+    delay(5);
+  }
+
+  delay(1000);
+
+  return true;
 
 }
+
+
+
+
+
+
 
 
 
